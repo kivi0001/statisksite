d5464e40b7ec787be80a0318b8b8b5a9e9b9217e
Kia <kivi0001@stud.kea.dk>
2025-09-23 13:34:09 +0200

    øver m js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const tid = new Date().getHours()
+
+if(tid < 5) { 
+    console.log("Godnat");
+} else if(tid < 10) {
+    console.log("Godmorgen");
+} else if(tid < 18) {
+    console.log("Goddag");
+} else {console.log("Godaften");
+}
